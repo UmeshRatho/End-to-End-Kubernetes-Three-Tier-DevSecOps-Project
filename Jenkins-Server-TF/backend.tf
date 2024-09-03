08f1bd6171ec528e98c82ed01ab469c03101aa98
Umesh Rathod <122077247+UmeshRatho@users.noreply.github.com>
2024-09-03 14:22:29 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,8 +3,8 @@ terraform {
     bucket         = "umes3bucket2024 "
     region         = "ap-south-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "terraform.statelock"
-    encrypt        = true
+ #  dynamodb_table = "terraform.statelock"
+#   encrypt        = true
   }
   required_version = ">=0.13.0"
   required_providers {
